01e3136e7a6d7d41748994ffa38ec5853f4de4be
LuisLeonM <luisleonm435@gmail.com>
2023-02-06 21:23:13 -0700

    Se modifica prueba para indexController

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -27,7 +32,23 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        //cuando se llame la funcion getRecipes de recipeService se enviara el set creado
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        //variable para capturar variables en verifys
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
         String viewName = indexController.getIndexPage(model);
+
+
 
         //se verifica que regrese el template correcto
         assertEquals(viewName, "index");
@@ -36,7 +57,9 @@ public class IndexControllerTest {
         verify(recipeService, times(1)).getRecipes();
 
         //se verifica que exista la variable recipes en el modelo
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
 
     }
 }
